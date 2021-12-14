196fc6edb297db95650d03efb7c5922f576ce7f1
Inna Chonka <chonka.inna.v@gmail.com>
2021-12-14 13:21:46 +0300

    fixes for hw2

--- src/main/java/ru/geekbrains/hw2/HomeWorkApp2.java
+++ src/main/java/ru/geekbrains/hw2/HomeWorkApp2.java
@@ -8,23 +8,27 @@ public class HomeWorkApp2 {
 
         result = checkSum(a,b);
         if (result){
-            System.out.println("The result of sum is within [10,20]");
+            System.out.println("The result of sum " + a + "+" + b + " is within [10,20]");
         } else {
-            System.out.println("The result of sum isn't within [10,20]");
+            System.out.println("The result of sum " + a + "+" + b + " isn't within [10,20]");
         }
+        System.out.println("*************");
 
         isPositive(a);
+        System.out.println("*************");
 
         result = isNegative(a);
         if (result){
-            System.out.println("The number is negative");
+            System.out.println("The number " + a + " is negative");
         } else {
-            System.out.println("The number is positive");
+            System.out.println("The number " + a + " is positive");
         }
+        System.out.println("*************");
 
-        int count = 5;
+        int count = 4;
         String str = "String for printing";
         printMultipleStrings(count, str);
+        System.out.println("*************");
 
         int year = 2000;
         result = isLeapYear(year);
@@ -34,33 +38,26 @@ public class HomeWorkApp2 {
     }
 
     private static boolean checkSum (int a, int b){
-        int sum;
-        sum = a + b;
-        return (sum>=10 && sum<=20) ? true : false;
+        return (a + b >=10) && (a + b <=20);
     }
 
     private static void isPositive(int a){
         String message;
-        message = (a>=0) ? "Number is positive" : "Number is negative";
+        message = (a>=0) ? a + " is positive" : a + " is negative";
         System.out.println(message);
     }
 
     private static boolean isNegative (int a){
-        return (a < 0) ? true : false;
+        return (a < 0);
     }
 
     private static void printMultipleStrings (int count, String str) {
-        if(count <= 0) {
-            System.out.println("Wrong count value in printMultipleStrings");
-            return;
-        }
-        for (int i = 0; i < count; i++){
-            System.out.println(str);
+        for (int i = 1; i <= count; i++){
+            System.out.println("String # "+ i + ": " + str);
         }
     }
 
     private static boolean isLeapYear(int year){
-        if(year%4==0 || (year%400==0 && year%100!=0)) return true;
-        return false;
+        return (year%4==0) && (year%100!=0) || (year%400==0);
     }
 }
